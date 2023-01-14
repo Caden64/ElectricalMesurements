0a9bdca5b203524dca2d6ea626b7dc6fbdb4ed31
Caden <cadenrothzeid@gmail.com>
2023-01-14 10:45:17 -0700

    Ohms fully tested

--- src/lib.rs
+++ src/lib.rs
@@ -90,6 +90,7 @@ mod tests {
         assert_eq!(Ohm(-5.0) + Ohm(3.0), Ohm(-2.0))
     }
     #[test]
+
     fn ohm_add_assign() {
         let mut test_value = Ohm(5.0);
         test_value += Ohm(3.0);
@@ -101,5 +102,77 @@ mod tests {
         let mut test_value = Ohm(-5.0);
         test_value += Ohm(3.0);
         assert_eq!(test_value, Ohm(-2.0))
+    }
+
+    #[test]
+    fn ohm_sub() {
+        assert_eq!(Ohm(5.0) - Ohm(3.0), Ohm(2.0))
+    }
+
+    #[test]
+    fn ohm_sub_neg() {
+        assert_eq!(Ohm(-5.0) - Ohm(3.0), Ohm(-8.0))
+    }
+
+    #[test]
+    fn  ohm_sub_assign() {
+        let mut test_value = Ohm(5.0);
+        test_value -= Ohm(3.0);
+        assert_eq!(test_value, Ohm(2.0))
+    }
+
+    #[test]
+    fn ohm_sub_assign_neg() {
+        let mut test_value = Ohm(-5.0);
+        test_value -= Ohm(3.0);
+        assert_eq!(test_value, Ohm(-8.0))
+    }
+
+    #[test]
+    fn ohm_mul() {
+        assert_eq!(Ohm(5.0) * Ohm(3.0), Ohm(15.0))
+    }
+
+    #[test]
+    fn ohm_mul_neg() {
+        assert_eq!(Ohm(-5.0) * Ohm(3.0), Ohm(-15.0))
+    }
+
+    #[test]
+    fn ohm_mul_assign() {
+        let mut test_value = Ohm(5.0);
+        test_value *= Ohm(3.0);
+        assert_eq!(test_value, Ohm(15.0))
+    }
+
+    #[test]
+    fn ohm_mul_assign_neg() {
+        let mut test_value = Ohm(-5.0);
+        test_value *= Ohm(3.0);
+        assert_eq!(test_value, Ohm(-15.0))
+    }
+
+    #[test]
+    fn ohm_div() {
+        assert_eq!(Ohm(5.0) / Ohm(3.0), Ohm(1.6666666666666667))
+    }
+
+    #[test]
+    fn ohm_div_neg() {
+        assert_eq!(Ohm(-5.0) / Ohm(3.0), Ohm(-1.6666666666666667))
+    }
+
+    #[test]
+    fn ohm_div_assign() {
+        let mut test_value = Ohm(5.0);
+        test_value /= Ohm(3.0);
+        assert_eq!(test_value, Ohm(1.6666666666666667))
+    }
+
+    #[test]
+    fn ohm_div_assign_neg() {
+        let mut test_value = Ohm(-5.0);
+        test_value /= Ohm(3.0);
+        assert_eq!(test_value, Ohm(-1.6666666666666667))
     }
 }
